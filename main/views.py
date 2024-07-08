4c9a8bd549cb90cff2da494cc53bc9312320646d
rsbtv <111227968+rsbtv@users.noreply.github.com>
2024-07-08 21:38:36 +0300

    about page added

--- main/views.py
+++ main/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
+
 
 # Create your views here.
+
+def other_page(request, page): # из page получаем имя выводимой страницы
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
+
+
+def index(request):
+    return render(request, 'main/index.html')
