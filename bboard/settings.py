a44cff4a0a7c0056468216f99b4e4635ecbe6bcf
rsbtv <111227968+rsbtv@users.noreply.github.com>
2024-07-08 15:44:52 +0300

    Lang changed to ru; db name changed to bboard.data

--- bboard/settings.py
+++ bboard/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'bboard.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'bboard.data',
     }
 }
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
