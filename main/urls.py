03d2a3861404c0a00ad48ae8896e815eaaab16c6
rsbtv <111227968+rsbtv@users.noreply.github.com>
2024-07-09 15:10:40 +0300

    syntax error fixed

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from .views import index, other_page, BBLoginView
 app_name = 'main'
 urlpatterns = [
     path('<str:page>/', other_page, name='other'),
-    path('', index, name='index')
+    path('', index, name='index'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
 ]
