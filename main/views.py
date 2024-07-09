96e27b45e9d543fff730598ce7d69049500e3173
rsbtv <111227968+rsbtv@users.noreply.github.com>
2024-07-09 15:10:09 +0300

    Login page added

--- main/views.py
+++ main/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-
+from django.contrib.auth.views import LoginView
 
 # Create your views here.
 
+
+class BBLoginView(LoginView):
+    template_name = 'main/login.html'
 def other_page(request, page): # из page получаем имя выводимой страницы
     try:
         template = get_template('main/' + page + '.html')
