10a3280039aa14eb432ca704c9b21e91fe55ad8d
rsbtv <111227968+rsbtv@users.noreply.github.com>
2024-07-11 14:35:13 +0300

    There`s an error when trying to logout

--- main/views.py
+++ main/views.py
@@ -1,15 +1,31 @@
+from django.contrib.auth import logout
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 # Create your views here.
 
 
 class BBLoginView(LoginView):
     template_name = 'main/login.html'
-def other_page(request, page): # из page получаем имя выводимой страницы
+
+
+class BBLogoutView(LoginRequiredMixin,
+                   LogoutView):  # если используется миксин, то видимо можно и от него наследовать методы
+    template_name = 'main/logout.html'
+
+
+@login_required()
+def profile(request):
+    return render(request, 'main/profile.html')
+
+
+def other_page(request, page):  # из page получаем имя выводимой страницы
     try:
         template = get_template('main/' + page + '.html')
     except TemplateDoesNotExist:
